dab95ce70d8db121a53eb07f7472d2880c11f231
Murilo Fink <mramosfink@gmail.com>
2026-05-28 13:25:51 -0300

    Update ChatModelController.java

--- DIO_Api_Voz/DIO_Api_Voz/src/main/java/dio/budgeting/ChatModelController.java
+++ DIO_Api_Voz/DIO_Api_Voz/src/main/java/dio/budgeting/ChatModelController.java
@@ -8,16 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ChatModelController {
-
     private final OpenAiChatModel openAiChatModel;
     public ChatModelController(OpenAiChatModel openAiChatModel){
         this.openAiChatModel = openAiChatModel;
     }
-
     @GetMapping("/chat-model")
     String chat(String prompt){
         return this.openAiChatModel.call(prompt);
     }
-
 }
 
