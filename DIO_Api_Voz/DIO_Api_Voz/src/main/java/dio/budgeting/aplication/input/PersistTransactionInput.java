91a74418728d2713d343edda103149b2bb2a4e48
Murilo Fink <mramosfink@gmail.com>
2026-05-28 13:24:00 -0300

    Update parameter descriptions in PersistTransactionInput

--- DIO_Api_Voz/DIO_Api_Voz/src/main/java/dio/budgeting/aplication/input/PersistTransactionInput.java
+++ DIO_Api_Voz/DIO_Api_Voz/src/main/java/dio/budgeting/aplication/input/PersistTransactionInput.java
@@ -3,9 +3,9 @@ package dio.budgeting.aplication.input;
 import dio.budgeting.domain.Category;
 import org.springframework.ai.tool.annotation.ToolParam;
 
-public record PersistTransactionInput(@ToolParam(description = "Descricao do gasto") String description,
-                                      @ToolParam(description = "Preco do gasto") Long amount,
-                                      @ToolParam(description = "Categoria do gasto") Category category) {
+public record PersistTransactionInput(@ToolParam(description = "Descricao para os gasto") String description,
+                                      @ToolParam(description = "Preco para os gasto") Long amount,
+                                      @ToolParam(description = "Categoria para os gasto") Category category) {
 
 
 }
