01d81631708cfe7c590a14297dd01735fff03e40
Murilo Fink <mramosfink@gmail.com>
2026-05-28 13:25:19 -0300

    Update TransactionEntity.java

--- DIO_Api_Voz/DIO_Api_Voz/src/main/java/dio/budgeting/infrastructer/persistence/entity/TransactionEntity.java
+++ DIO_Api_Voz/DIO_Api_Voz/src/main/java/dio/budgeting/infrastructer/persistence/entity/TransactionEntity.java
@@ -1,6 +1,5 @@
 package dio.budgeting.infrastructer.persistence.entity;
 
-
 import dio.budgeting.domain.Category;
 import dio.budgeting.domain.Transaction;
 import dio.budgeting.domain.TransactionId;
@@ -11,7 +10,6 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
 @Entity
@@ -26,15 +24,12 @@ public class TransactionEntity {
 
     @Enumerated(EnumType.STRING)
     private Category category;
-
     public static TransactionEntity from(Transaction transaction){
         return new  TransactionEntity(transaction.getId().uuid(),
                 transaction.getDescription(),
                 transaction.getAmount(),
                 transaction.getCategory());
     }
-
-
     public Transaction toDomain() {
         return new Transaction(
                 new TransactionId(this.Id),this.description, this.amount, this.category
